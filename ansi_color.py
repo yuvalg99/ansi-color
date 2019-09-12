ca28c1963bf9693d923e0f9352ab9add2bdf1366
Yuval Goihberg <yuvalyoav7@gmail.com>
2019-09-12 22:28:02 +0300

    Fixed bugs

--- ansi_color.py
+++ ansi_color.py
@@ -1,7 +1,9 @@
 import foreground
+import background
+import style
 
 def color(style, foreground_color, background_color):
-    return "\033[{style},{foreground_color},{background_color}".format(
+    return "\033[{style};{foreground_color};{background_color}m".format(
         style = style,
         foreground_color = foreground_color,
         background_color = background_color
---
+++ style.py
@@ -0,0 +1,7 @@
+Normal = 0
+Bold = 1
+Italian = 3
+Underline = 4
+Blincked = 5
+Reversed = 7
+StrikeThrough = 9
